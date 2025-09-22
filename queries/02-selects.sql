7d73ede8a65b40119433c9b5ac7b5b5ec9eed75e
Fabio Trevizolli <fabiorabello08@gmail.com>
2025-09-22 09:17:15 -0300

    delete and count

--- queries/02-selects.sql
+++ queries/02-selects.sql
@@ -9,3 +9,8 @@ SELECT nome, genero_musical FROM artistas WHERE pais_origem = 'Reino Unido';
 SELECT nome, genero_musical FROM artistas WHERE id = 7;
 
 SELECT nome, genero_musical FROM artistas WHERE id = 7 AND id = 10;
+
+
+
+
+SELECT COUNT(*) AS total_usuarios FROM usuarios;
